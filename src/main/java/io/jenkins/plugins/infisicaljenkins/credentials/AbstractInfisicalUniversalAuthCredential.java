abef21c2873359f6f712fef56e219d51804def77
Daniel Hougaard <62331820+DanielHougaard@users.noreply.github.com>
2024-05-15 17:01:30 +0200

    Rename

--- src/main/java/io/jenkins/plugins/infisicaljenkins/credentials/AbstractInfisicalUniversalAuthCredential.java
+++ src/main/java/io/jenkins/plugins/infisicaljenkins/credentials/AbstractInfisicalUniversalAuthCredential.java
@@ -3,10 +3,10 @@ package io.jenkins.plugins.infisicaljenkins.credentials;
 import com.cloudbees.plugins.credentials.CredentialsScope;
 import com.cloudbees.plugins.credentials.impl.BaseStandardCredentials;
 
-public abstract class AbstractInfisicalTokenCredential
+public abstract class AbstractInfisicalUniversalAuthCredential
     extends BaseStandardCredentials implements InfisicalCredential {
 
-  protected AbstractInfisicalTokenCredential(CredentialsScope scope, String id, String description) {
+  protected AbstractInfisicalUniversalAuthCredential(CredentialsScope scope, String id, String description) {
     super(scope, id, description);
   }
 }
